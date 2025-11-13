ac49a4f39693b6ee5fabd8b20e648c7674d3e671
kiranrawat <rawat.kiran111@gmail.com>
2025-11-12 21:54:03 -0800

    added solution for linkedlist leetcode problem

---
+++ Linked_List/MergeSortedList.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        tail.next = list1 if list1 else list2
+        return dummy.next
+
+# Runtime: O(n + m)
+# Memory: O(1) (only pointers changed)
