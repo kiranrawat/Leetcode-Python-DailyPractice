b075c6a7856010dffd69106d9d8b31ce7313849d
kiranrawat <rawat.kiran111@gmail.com>
2025-11-28 19:17:56 -0500

    two pointer solution

--- Easy/String/nt_merge_strings_alternately.py
+++ Easy/String/nt_merge_strings_alternately.py
@@ -16,3 +16,23 @@ class Solution:
 
 # time complexity - O(n)
 # space complexity - O(n)
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        n = len(word1)
+        m = len(word2)
+        res = []
+        i = j = 0
+        while i < n or j < m:
+            if i < n:
+                res.append(word1[i])
+            if j < m:
+                res.append(word2[j])
+            i += 1
+            j += 1
+
+        return "".join(res)
+
+# time complexity - O(n + m)
+# space complexity - O(n + m)
