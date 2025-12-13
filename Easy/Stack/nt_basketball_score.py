196c62294ec49f1aa5878a901a12d6f0bcdce4fb
kiranrawat <rawat.kiran111@gmail.com>
2025-12-13 08:29:44 -0800

    neetcode stack basetball score calculation

---
+++ Easy/Stack/nt_basketball_score.py
@@ -0,0 +1,15 @@
+def calPoints(self, operations: List[str]) -> int:
+    stack = []
+    for op in operations:
+        if op == '+':
+            stack.append(stack[-1] + stack[-2])
+        elif op == 'D':
+            stack.append(stack[-1] * 2)
+        elif op == 'C':
+            stack.pop()
+        else:
+            stack.append(int(op))
+    return sum(stack)
+
+# time complexity - O(n)
+# space complexity - O(n)
