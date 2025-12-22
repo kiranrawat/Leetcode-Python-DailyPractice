159a90836efe597a628f750af01c99b794c47b4b
kiranrawat <rawat.kiran111@gmail.com>
2025-12-21 22:39:52 -0800

    leetcode merge sorted arrays inplace

---
+++ Easy/Array/lt_merge_sorted_arrays.py
@@ -0,0 +1,24 @@
+def merge(self, nums1, m, nums2, n):
+    """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+    """
+    # nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+# time complexity - O(m+n)
+# space complexity - O(1)
