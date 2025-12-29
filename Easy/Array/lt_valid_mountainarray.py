df1d38405a145a9a1e1e0b09f0aed0ac3e70a27e
kiranrawat <rawat.kiran111@gmail.com>
2025-12-29 15:20:44 -0800

    valid mountain array

---
+++ Easy/Array/lt_valid_mountainarray.py
@@ -0,0 +1,25 @@
+def validMountainArray(self, arr):
+    """
+    :type arr: List[int]
+    :rtype: bool
+    """
+    # [0,3,2,1]
+    n = len(arr)
+    if n < 3:
+        return False
+
+    i = 0
+
+    # walk up
+    while i + 1 < n and arr[i] < arr[i + 1]:
+        i += 1
+
+    # peak can't be first or last
+    if i == 0 or i == n - 1:
+        return False
+
+    # walk down
+    while i + 1 < n and arr[i] > arr[i + 1]:
+        i += 1
+
+    return i == n - 1
