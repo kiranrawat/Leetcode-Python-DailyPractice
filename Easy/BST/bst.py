e5589cc207ed275437050f25a0e885495897a857
kiranrawat <rawat.kiran111@gmail.com>
2026-01-10 19:16:29 -0800

    bst implementation

---
+++ Easy/BST/bst.py
@@ -0,0 +1,15 @@
+def search(self, nums: List[int], target: int) -> int:
+    # nums = [-1,0,2,4,6,8], target = 4
+
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if target == nums[mid]:
+            return mid
+        elif target < nums[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
