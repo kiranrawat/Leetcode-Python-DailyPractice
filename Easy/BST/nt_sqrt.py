00154816b0a6884e79bbd0b0ac272ca8d4bef690
kiranrawat <rawat.kiran111@gmail.com>
2026-01-13 14:01:44 -0800

    brute force

--- Easy/BST/nt_sqrt.py
+++ Easy/BST/nt_sqrt.py
@@ -37,3 +37,20 @@ def mySqrt(self, x: int) -> int:
             res = mid
 
     return res
+
+
+# brute force solution
+
+def mySqrt(self, x: int) -> int:
+    if x == 0:
+        return 0
+
+    res = 1
+    for i in range(1, x+1):
+        if i*i > x:
+            return res
+        res = i
+    return res
+
+# time complexity - O(sqrt(n))
+# space complexity - O(1)
