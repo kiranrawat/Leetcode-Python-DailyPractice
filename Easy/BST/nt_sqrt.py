18fe2e4993fd170e54e7f947337d93938f757835
kiranrawat <rawat.kiran111@gmail.com>
2026-01-13 13:19:47 -0800

    another solution

--- Easy/BST/nt_sqrt.py
+++ Easy/BST/nt_sqrt.py
@@ -17,3 +17,23 @@ def mySqrt(self, x: int) -> int:
 
 # time complexity - O(logn)
 # space complexity - O(1)
+
+
+# another solution (slight different)
+
+def mySqrt(self, x: int) -> int:
+    low = 0
+    high = x
+    res = 0
+
+    while low <= high:
+        mid = low + ((high - low) // 2)  # never overflow
+        if mid**2 == x:
+            return mid
+        elif mid**2 > x:
+            high = mid - 1
+        else:
+            low = mid + 1
+            res = mid
+
+    return res
